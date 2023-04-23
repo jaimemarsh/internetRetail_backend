4d8a872f2e1df0acf0a924686f8997ba0de1d6f0
Jaime M <jaimemarsh19@gmail.com>
2023-04-23 15:04:06 -0400

    only did api/product-routes findAll and findOne, unsure if create and update need to be changed from given code

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -20,8 +20,19 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category, {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.statys(400).json(err))
 });
 
 // create new product
