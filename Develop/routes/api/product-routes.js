43da67abec3848ebe6757752b63f70fb7627b2d1
Jaime M <jaimemarsh19@gmail.com>
2023-04-23 14:58:18 -0400

    products findAll, referenced categories I just completed. Need to check

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,8 +5,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.statys(500).json(err))
 });
 
 // get one product
